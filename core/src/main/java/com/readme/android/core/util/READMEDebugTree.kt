b79bd0c73d0e912d254a5531623c6d5c2289af12
mdb1217 <70698151+mdb1217@users.noreply.github.com>
2022-04-21 02:08:22 +0900

    [chore] 팀버용 READMEDebugTree 초기화 (#1)

---
+++ core/src/main/java/com/readme/android/core/util/READMEDebugTree.kt
@@ -0,0 +1,9 @@
+package com.readme.android.core.util
+
+import timber.log.Timber
+
+class READMEDebugTree : Timber.DebugTree() {
+    override fun createStackElementTag(element: StackTraceElement): String? {
+        return "${element.fileName}:${element.lineNumber}#${element.methodName}"
+    }
+}
