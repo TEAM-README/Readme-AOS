497c47b48fd7205ecc26b3e9c20f58cb9a938da1
mdb1217 <70698151+mdb1217@users.noreply.github.com>
2022-04-21 02:04:27 +0900

    [chore] build.gradle 파일 각 모듈마다 초기화 (#1)

---
+++ app/build.gradle.kts
@@ -0,0 +1,107 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-parcelize")
+    kotlin("android")
+    kotlin("kapt")
+    kotlin("plugin.serialization") version Versions.kotlinVersion
+    id("org.jlleitschuh.gradle.ktlint") version Versions.ktlintVersion
+    id("dagger.hilt.android.plugin")
+}
+
+android {
+    buildFeatures {
+        dataBinding = true
+    }
+}
+
+dependencies {
+    implementation(project(":core"))
+    implementation(project(":feature:home"))
+    implementation(project(":data"))
+    implementation(project(":domain"))
+
+    // Kotlin
+    implementation(KotlinDependencies.kotlin)
+    implementation(KotlinDependencies.kotlinxSerialization)
+    implementation(KotlinDependencies.dateTime)
+
+    // Android Core
+    implementation(AndroidXDependencies.coreKtx)
+    implementation(AndroidXDependencies.appCompat)
+    implementation(AndroidXDependencies.constraintLayout)
+    implementation(AndroidXDependencies.legacy)
+    implementation(AndroidXDependencies.coroutines)
+    implementation(AndroidXDependencies.paging3)
+    coreLibraryDesugaring(AndroidXDependencies.desugarLibrary)
+
+    // Material Design
+    implementation(MaterialDesignDependencies.materialDesign)
+
+    // Dagger-Hilt
+    implementation(AndroidXDependencies.hilt)
+    kapt(KaptDependencies.hiltCompiler)
+
+    // Jetpack Navigation Component
+    implementation(AndroidXDependencies.navigationFragment)
+    implementation(AndroidXDependencies.navigationUI)
+
+    // Jetpack Security
+    implementation(AndroidXDependencies.security)
+
+    // Jetpack Fragment
+    implementation(AndroidXDependencies.fragment)
+
+    // Jetpack Lifecycle
+    implementation(AndroidXDependencies.coroutines)
+    implementation(AndroidXDependencies.lifeCycleKtx)
+    implementation(AndroidXDependencies.lifecycleJava8)
+
+    // ImageLoading Library
+    // Glide for general
+    // Coil for compose
+    implementation(ThirdPartyDependencies.glide)
+    kapt(KaptDependencies.glideCompiler)
+    implementation(ThirdPartyDependencies.coil)
+
+    // Http Client Library
+    implementation(ThirdPartyDependencies.retrofit)
+    implementation(platform(ThirdPartyDependencies.okHttpBom))
+    implementation(ThirdPartyDependencies.okHttp)
+    implementation(ThirdPartyDependencies.okHttpLoggingInterceptor)
+    implementation(ThirdPartyDependencies.kotlinxSerializationConverter)
+
+    // Logger - Timber
+    implementation(ThirdPartyDependencies.timber)
+
+    // Automatic Record OpenSource Library List
+    implementation(ThirdPartyDependencies.ossLicense)
+
+    // Firebase
+    implementation(platform(FirebaseDependency.firebaseBom))
+    implementation(FirebaseDependency.analyticsKtx)
+    implementation(FirebaseDependency.crashlyticsKtx)
+
+    // Flipper
+    debugImplementation(ThirdPartyDependencies.flipper)
+    debugImplementation(ThirdPartyDependencies.flipperNetwork)
+    debugImplementation(ThirdPartyDependencies.soloader)
+    debugImplementation(ThirdPartyDependencies.flipperLeakCanary)
+    debugImplementation(ThirdPartyDependencies.leakCanary)
+
+    // Test Dependency
+    testImplementation(TestDependencies.androidTest)
+    testImplementation(TestDependencies.jUnit)
+    testImplementation(TestDependencies.espresso)
+}
+
+ktlint {
+    android.set(true)
+    coloredOutput.set(true)
+    verbose.set(true)
+    outputToConsole.set(true)
+    disabledRules.set(setOf("max-line-length", "no-wildcard-imports", "import-ordering"))
+    reporters {
+        reporter(org.jlleitschuh.gradle.ktlint.reporter.ReporterType.PLAIN)
+        reporter(org.jlleitschuh.gradle.ktlint.reporter.ReporterType.CHECKSTYLE)
+    }
+}
