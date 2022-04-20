7f2b40888f3adb3ba1dcb329b9c5367b162c0fcf
mdb1217 <70698151+mdb1217@users.noreply.github.com>
2022-04-21 02:24:07 +0900

    [add] AppModule 추가 (#1)

---
+++ app/src/main/java/com/readme/android/di/AppModule.kt
@@ -0,0 +1,18 @@
+package com.readme.android.di
+
+import android.app.Application
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+    @Provides
+    @Singleton
+    @ApplicationContext
+    fun provideApplication(app: Application) = app
+}
