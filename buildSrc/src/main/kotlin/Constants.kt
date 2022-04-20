c59383d60f3fd4c4a9984c93c7ec1aab9e93d38b
mdb1217 <70698151+mdb1217@users.noreply.github.com>
2022-04-21 02:06:09 +0900

    [chore] buildSrc모듈 초기화 (#1)

---
+++ buildSrc/src/main/kotlin/Constants.kt
@@ -0,0 +1,8 @@
+object Constants {
+    const val packageName = "com.valuecruncher.gachisaja"
+    const val compileSdk = 32
+    const val minSdk = 23
+    const val targetSdk = 32
+    const val versionCode = 1
+    const val versionName = "1.0"
+}
