2720417fdc3c09386dcff2c1ae8ce42d736d2f3b
mdb1217 <70698151+mdb1217@users.noreply.github.com>
2022-05-03 03:30:55 +0900

    [chore] main에서 필요 없는 의존성 제거 (#6)

--- feature/main/build.gradle.kts
+++ feature/main/build.gradle.kts
@@ -28,9 +28,6 @@ dependencies {
 
     // Dagger-Hilt
     implementation(AndroidXDependencies.hilt)
-    implementation("androidx.appcompat:appcompat:1.4.1")
-    implementation("com.google.android.material:material:1.5.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.3")
     kapt(KaptDependencies.hiltCompiler)
 
     // Jetpack Lifecycle
