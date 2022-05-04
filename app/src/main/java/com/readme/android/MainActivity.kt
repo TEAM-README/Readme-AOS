75d10462dfbc863d7d7ac36cdcf8cddcc9c7fce8
Kim Hyo Rim <rlagyfla0709@gmail.com>
2022-05-05 05:06:26 +0900

    [feat] MainViewPagerAdapter 생성 및 초기화(#27)

--- app/src/main/java/com/readme/android/MainActivity.kt
+++ app/src/main/java/com/readme/android/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.readme.android
 
 import android.os.Bundle
+import com.readme.android.adapter.MainViewPagerAdapter
 import com.readme.android.core.base.BindingActivity
 import com.readme.android.databinding.ActivityMainBinding
+import com.readme.android.main.ui.home.HomeFragment
+import com.readme.android.main.ui.mypage.MyPageFragment
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main) {
+    private lateinit var mainViewPagerAdapter: MainViewPagerAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        initAdapter()
+    }
+
+    private fun initAdapter() {
+        binding.vpMain.adapter = MainViewPagerAdapter(this).also { mainViewPagerAdapter = it }
+        mainViewPagerAdapter.fragments.addAll(listOf(HomeFragment(), MyPageFragment()))
     }
 }
---
+++ app/src/main/java/com/readme/android/adapter/MainViewPagerAdapter.kt
@@ -0,0 +1,11 @@
+package com.readme.android.adapter
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class MainViewPagerAdapter(fa: FragmentActivity) : FragmentStateAdapter(fa) {
+    val fragments = mutableListOf<Fragment>()
+    override fun getItemCount(): Int = fragments.size
+    override fun createFragment(position: Int): Fragment = fragments[position]
+}
