44e240fad0bb897f46fffe446a8b54823b51882e
Kim Hyo Rim <rlagyfla0709@gmail.com>
2022-05-05 06:42:54 +0900

    [feat] activity_main / BottomNavigation과 ViewPager 연동 (#29)

--- app/src/main/java/com/readme/android/MainActivity.kt
+++ app/src/main/java/com/readme/android/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.readme.android
 
 import android.os.Bundle
+import androidx.viewpager2.widget.ViewPager2
 import com.readme.android.adapter.MainViewPagerAdapter
 import com.readme.android.core.base.BindingActivity
 import com.readme.android.databinding.ActivityMainBinding
@@ -14,10 +15,28 @@ class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         initAdapter()
+        syncVpWithBottomNav()
     }
 
     private fun initAdapter() {
         binding.vpMain.adapter = MainViewPagerAdapter(this).also { mainViewPagerAdapter = it }
         mainViewPagerAdapter.fragments.addAll(listOf(HomeFragment(), MyPageFragment()))
+    }
+
+    private fun syncVpWithBottomNav() {
+        binding.vpMain.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                binding.bottomNav.menu.getItem(if (position == 0) 0 else 2).isChecked = true
+            }
+        })
+
+        binding.bottomNav.setOnItemSelectedListener {
+            binding.vpMain.currentItem = when (it.itemId) {
+                R.id.menu_home -> 0
+                R.id.menu_my_page -> 1
+                else -> return@setOnItemSelectedListener false
+            }
+            return@setOnItemSelectedListener true
+        }
     }
 }
