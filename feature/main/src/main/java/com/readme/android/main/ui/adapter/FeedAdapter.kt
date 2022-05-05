1209b411d56b78b0ff96c07d321e41ba3eb9d4cb
mdb1217 <70698151+mdb1217@users.noreply.github.com>
2022-05-06 04:02:55 +0900

    [feat] 피드 어댑터 1차 구현 (#26)

---
+++ feature/main/src/main/java/com/readme/android/main/ui/adapter/FeedAdapter.kt
@@ -0,0 +1,45 @@
+package com.readme.android.main.ui.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.constraintlayout.widget.ConstraintLayout.LayoutParams
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.readme.android.core.util.ItemDiffCallback
+import com.readme.android.domain.entity.Feed
+import com.readme.android.main.databinding.ItemFeedBinding
+
+class FeedAdapter : ListAdapter<Feed, FeedAdapter.FeedViewHolder>(
+    ItemDiffCallback<Feed>(
+        onContentsTheSame = { old, new -> old == new },
+        onItemsTheSame = { old, new -> old == new }
+    )
+) {
+    private lateinit var inflater: LayoutInflater
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FeedViewHolder {
+        if (!::inflater.isInitialized)
+            inflater = LayoutInflater.from(parent.context)
+
+        val binding = ItemFeedBinding.inflate(inflater, parent, false)
+        return FeedViewHolder(binding.apply {
+            root.layoutParams = LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+            ).apply {
+                setMargins(0, 0, 18, 0)
+            }
+        })
+    }
+
+    override fun onBindViewHolder(holder: FeedViewHolder, position: Int) {
+        holder.onBind(getItem(position))
+    }
+
+    class FeedViewHolder(private val binding: ItemFeedBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun onBind(feed: Feed) {
+            binding.feedData = feed
+        }
+    }
+}
