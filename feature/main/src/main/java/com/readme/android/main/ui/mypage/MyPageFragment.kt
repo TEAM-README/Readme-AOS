c86a41c41db31577391160a653acf144e510d3c5
mdb1217 <70698151+mdb1217@users.noreply.github.com>
2022-05-06 04:03:46 +0900

    [feat] 마이페이지 / MyPageFragment에 콘캣어댑터 로직 추가 (#26)

--- feature/main/src/main/java/com/readme/android/main/ui/mypage/MyPageFragment.kt
+++ feature/main/src/main/java/com/readme/android/main/ui/mypage/MyPageFragment.kt
@@ -2,18 +2,71 @@ package com.readme.android.main.ui.mypage
 
 import android.os.Bundle
 import android.view.View
+import androidx.recyclerview.widget.ConcatAdapter
 import com.readme.android.core.base.BindingFragment
+import com.readme.android.core.util.ItemDecorationUtil
+import com.readme.android.domain.entity.Feed
+import com.readme.android.domain.entity.User
 import com.readme.android.main.R
 import com.readme.android.main.databinding.FragmentMyPageBinding
+import com.readme.android.main.ui.adapter.FeedAdapter
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MyPageFragment : BindingFragment<FragmentMyPageBinding>(R.layout.fragment_my_page) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        initAdapter()
     }
 
     private fun initAdapter() {
-        // TODO 콘캣 어댑터가 들어갈거에용
+        // TODO 데이터 연동 로직 + adapter 변수 스코프 수정
+        val myPageTopAdapter = MyPageTopAdapter(User(Int.MAX_VALUE, "문다빙빙테스트용", 5))
+        val feedAdapter = FeedAdapter()
+        val concatAdapter = ConcatAdapter(
+            myPageTopAdapter,
+            feedAdapter.apply {
+                submitList(
+                    listOf(
+                        Feed(
+                            0,
+                            "에세이",
+                            "1cm 다빙빙",
+                            "문다빈 그는 신인가?",
+                            "개발 죽고싶넹",
+                            "문다빙빙테스트용",
+                            "2021/10/31",
+                            true
+                        ),
+                        Feed(
+                            0,
+                            "에세이",
+                            "1cm 다빙빙",
+                            "문다빈 그는 신인가?",
+                            "개발 죽고싶넹",
+                            "문다빙빙테스트용",
+                            "2021/10/31",
+                            true
+                        ),
+                        Feed(
+                            0,
+                            "에세이",
+                            "1cm 다빙빙",
+                            "문다빈 그는 신인가?",
+                            "개발 죽고싶넹",
+                            "문다빙빙테스트용",
+                            "2021/10/31",
+                            true
+                        )
+                    )
+                )
+            }
+        )
+
+        binding.rvMypage.apply {
+            addItemDecoration(ItemDecorationUtil.VerticalPlaceItemDecoration(16.dp))
+            adapter = concatAdapter
+        }
     }
 }
