28d5b264d4e548110f8173e182081a74029e969e
mdb1217 <70698151+mdb1217@users.noreply.github.com>
2022-05-08 22:16:56 +0900

    [feat] 프래그먼트 팩토리 추가해서 resolutionMetrics 필요할때만 주입하기 (#26)

---
+++ app/src/main/java/com/readme/android/factory/ReadmeFragmentFactory.kt
@@ -0,0 +1,25 @@
+package com.readme.android.factory
+
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentFactory
+import com.readme.android.core.util.Injector
+import com.readme.android.core.util.ResolutionMetrics
+import com.readme.android.main.ui.mypage.MyPageFragment
+import dagger.hilt.android.EntryPointAccessors
+
+class ReadmeFragmentFactory(activity: AppCompatActivity) : FragmentFactory() {
+    private val resolutionMetrics: ResolutionMetrics by lazy {
+        EntryPointAccessors.fromActivity(
+            activity,
+            Injector.ResolutionMetricsInjector::class.java
+        ).resolutionMetrics()
+    }
+
+    override fun instantiate(classLoader: ClassLoader, className: String): Fragment {
+        return when (className) {
+            MyPageFragment::class.java.name -> MyPageFragment(resolutionMetrics)
+            else -> super.instantiate(classLoader, className)
+        }
+    }
+}
