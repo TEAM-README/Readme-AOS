62a548f0cdbe576c904cc507698883a9182234a8
mdb1217 <70698151+mdb1217@users.noreply.github.com>
2022-05-08 22:17:16 +0900

    [add] getDimen 리소스 유틸 추가 (#26)

--- core/src/main/java/com/readme/android/core/ext/ResourceExtension.kt
+++ core/src/main/java/com/readme/android/core/ext/ResourceExtension.kt
@@ -2,6 +2,7 @@ package com.readme.android.core.ext
 
 import android.view.View
 import androidx.annotation.ColorRes
+import androidx.annotation.DimenRes
 import androidx.annotation.StringRes
 import androidx.fragment.app.Fragment
 
@@ -9,3 +10,4 @@ fun View.getColor(@ColorRes res: Int) = context.getColor(res)
 fun Fragment.getColor(@ColorRes res: Int) = requireContext().getColor(res)
 fun View.getString(@StringRes res: Int) = context.getString(res)
 fun Fragment.getString(@StringRes res: Int) = requireContext().getString(res)
+fun View.getDimen(@DimenRes res: Int) = context.resources.getDimension(res).toInt()
