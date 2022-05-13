48482b5ded44e374fa25584cce3a82f207da27d9
Kim Hyo Rim <rlagyfla0709@gmail.com>
2022-05-14 00:13:35 +0900

    [add] fragmnet_filter_bottom_sheet 추가 (#44)

---
+++ feature/main/src/main/java/com/readme/android/main/ui/home/FilterBottomSheetFragment.kt
@@ -0,0 +1,44 @@
+package com.readme.android.main.ui.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.readme.android.main.databinding.FragmentFilterBottomSheetBinding
+
+class FilterBottomSheetFragment : BottomSheetDialogFragment() {
+    private var _binding: FragmentFilterBottomSheetBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentFilterBottomSheetBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        setBottomSheetHeight()
+    }
+
+    private fun setBottomSheetHeight() {
+        (dialog as BottomSheetDialog).behavior.apply {
+            state = BottomSheetBehavior.STATE_EXPANDED // 높이 고정
+            skipCollapsed = true // HALF_EXPANDED 안되게 설정
+        }
+
+        binding.root.layoutParams.height =
+            (resources.displayMetrics.heightPixels * 0.67).toInt()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+        requireActivity().finish()
+    }
+}
