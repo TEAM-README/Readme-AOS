5bd35d39a58304da5059c9535fb98fb67f606b98
Kim Hyo Rim <rlagyfla0709@gmail.com>
2022-05-15 03:13:34 +0900

    [feat] FilterBottomSheetFragment 완료~!!!

--- core/src/main/java/com/readme/android/core/ext/ResourceExtension.kt
+++ core/src/main/java/com/readme/android/core/ext/ResourceExtension.kt
@@ -1,11 +1,11 @@
 package com.readme.android.core.ext
 
 import android.content.Context
-import androidx.core.content.ContextCompat
 import android.view.View
 import androidx.annotation.ColorRes
 import androidx.annotation.DimenRes
 import androidx.annotation.StringRes
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 
 fun View.getColor(@ColorRes res: Int) = context.getColor(res)
