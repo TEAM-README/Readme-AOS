7376b06ffa820b5f83366e54c0f18519f1e13a27
changhwan <chang5hustle@gmail.com>
2022-06-11 01:13:13 +0900

    [fix] 코드리뷰 반영 / project build.gradle 수정 (#51)

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ buildscript {
     }
     dependencies {
         classpath("com.android.tools.build:gradle:${Versions.gradleVersion}")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.21")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlinVersion}")
         classpath("com.google.gms:google-services:${Versions.googleServiceVersion}")
         classpath(ClassPathPlugins.crashlytics)
         classpath(ClassPathPlugins.hilt)
