68b81be5f776839d2187b5c29af5668705e0d4b9
mdb1217 <70698151+mdb1217@users.noreply.github.com>
2022-06-21 19:23:59 +0900

    [add] 네비게이터 추가 / auth 모듈 추가 (#49)

---
+++ feature/auth/build.gradle.kts
@@ -0,0 +1,40 @@
+plugins {
+    id("com.android.library")
+    kotlin("android")
+    kotlin("kapt")
+    id("dagger.hilt.android.plugin")
+}
+
+android {
+    buildFeatures {
+        dataBinding = true
+    }
+    namespace = "com.readme.android.auth"
+}
+
+
+dependencies {
+    implementation(project(":core-ui"))
+    implementation(project(":domain"))
+    implementation(project(":shared"))
+    implementation(project(":navigator"))
+
+    // Android Core
+    implementation(AndroidXDependencies.coreKtx)
+    implementation(AndroidXDependencies.appCompat)
+    implementation(AndroidXDependencies.constraintLayout)
+    implementation(AndroidXDependencies.coroutines)
+
+    // Material Design
+    implementation(MaterialDesignDependencies.materialDesign)
+
+    // Dagger-Hilt
+    implementation(AndroidXDependencies.hilt)
+    kapt(KaptDependencies.hiltCompiler)
+
+    // Jetpack Fragment
+    implementation(AndroidXDependencies.fragment)
+
+    // Logger - Timber
+    implementation(ThirdPartyDependencies.timber)
+}
