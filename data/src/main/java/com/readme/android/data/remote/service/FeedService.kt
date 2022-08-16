4acc13e926af4e038641dc2245e217593ed3adce
kxxhyorim <rlagyfla0709@gmail.com>
2022-08-16 18:33:59 +0900

    [chore] FeedService / query ? 제거 (#82)

--- data/src/main/java/com/readme/android/data/remote/service/FeedService.kt
+++ data/src/main/java/com/readme/android/data/remote/service/FeedService.kt
@@ -8,7 +8,7 @@ import retrofit2.http.Query
 
 interface FeedService {
 
-    @GET("feed?")
+    @GET("feed")
     suspend fun getHomeFeedList(
         @Query("filters") filters: String
     ): NetworkState<BaseResponse<HomeFeedResponse>>
