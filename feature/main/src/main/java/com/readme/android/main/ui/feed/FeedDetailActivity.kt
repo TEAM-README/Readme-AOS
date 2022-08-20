20bad4d62cb89697a1298e8f0411a843b4ddf06a
kxxhyorim <rlagyfla0709@gmail.com>
2022-08-21 06:46:35 +0900

    [feat] FeedDetailActivity.kt / 서버 연동 코드 로직 추가 (#91)

--- feature/main/src/main/java/com/readme/android/main/ui/feed/FeedDetailActivity.kt
+++ feature/main/src/main/java/com/readme/android/main/ui/feed/FeedDetailActivity.kt
@@ -1,23 +1,33 @@
 package com.readme.android.main.ui.feed
 
-import android.content.ContentValues.TAG
 import android.os.Bundle
-import android.util.Log
+import androidx.activity.viewModels
 import com.readme.android.core_ui.base.BindingActivity
 import com.readme.android.main.R
 import com.readme.android.main.databinding.ActivityFeedDetailBinding
+import com.readme.android.main.viewmodel.DetailFeedViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class FeedDetailActivity : BindingActivity<ActivityFeedDetailBinding>(R.layout.activity_feed_detail) {
+    private val viewModel : DetailFeedViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        getFeedIdFromExtra()
+        setFeedIdOnViewModelFromExtra()
+        getDetailFeedInfo()
     }
 
-    private fun getFeedIdFromExtra(){
-        val feedId = intent.getIntExtra(FEED_ID, -1)
-        Log.d(TAG, "getFeedIdFromExtra: $feedId")
+    private fun setFeedIdOnViewModelFromExtra(){
+        val feedId = getFeedIdFromExtra()
+        viewModel.setFeedId(feedId)
     }
+
+    private fun getDetailFeedInfo(){
+        viewModel.getDetailFeedInfo()
+    }
+
+    private fun getFeedIdFromExtra() : Int = intent.getIntExtra(FEED_ID, -1)
 
     companion object{
         const val FEED_ID = "FEED_ID"
