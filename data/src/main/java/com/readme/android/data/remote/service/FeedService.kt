55527c40288993569a5f7871e57faba925808f73
kxxhyorim <rlagyfla0709@gmail.com>
2022-08-21 06:43:32 +0900

    [feat] Detail Feed 조회 service 코드 추가 (#91)

--- data/src/main/java/com/readme/android/data/remote/service/FeedService.kt
+++ data/src/main/java/com/readme/android/data/remote/service/FeedService.kt
@@ -2,6 +2,7 @@ package com.readme.android.data.remote.service
 
 import com.readme.android.data.remote.calladapter.NetworkState
 import com.readme.android.data.remote.model.response.BaseResponse
+import com.readme.android.data.remote.model.response.DetailFeedResponse
 import com.readme.android.data.remote.model.response.HomeFeedResponse
 import retrofit2.http.Body
 import retrofit2.http.GET
@@ -15,8 +16,8 @@ interface FeedService {
         @Query("filters") filters: String
     ): NetworkState<BaseResponse<HomeFeedResponse>>
 
-//    @GET("/feed/{feedId}")
-//    suspend fun getFeedDetail(
-//        @Path("feedId") feedId : Int
-//    ): NetworkState<BaseResponse<>>
+    @GET("/feed/{feedId}")
+    suspend fun getDetailFeed(
+        @Path("feedId") feedId : Int
+    ): NetworkState<BaseResponse<DetailFeedResponse>>
 }
