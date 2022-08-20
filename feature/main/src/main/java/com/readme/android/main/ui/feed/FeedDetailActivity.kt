e493f23c1a91014e605831ab56124081f3ba9082
kxxhyorim <rlagyfla0709@gmail.com>
2022-08-21 05:44:18 +0900

    [feat] FeedDetailActivity / getExtra Data (#91)

--- feature/main/src/main/java/com/readme/android/main/ui/feed/FeedDetailActivity.kt
+++ feature/main/src/main/java/com/readme/android/main/ui/feed/FeedDetailActivity.kt
@@ -1,14 +1,22 @@
 package com.readme.android.main.ui.feed
 
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
 import com.readme.android.core_ui.base.BindingActivity
 import com.readme.android.main.R
 import com.readme.android.main.databinding.ActivityFeedDetailBinding
 
-class FeedDetailActivity :
-    BindingActivity<ActivityFeedDetailBinding>(R.layout.activity_feed_detail) {
+class FeedDetailActivity : BindingActivity<ActivityFeedDetailBinding>(R.layout.activity_feed_detail) {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        getFeedIdFromExtra()
+    }
+
+    private fun getFeedIdFromExtra(){
+        val feedId = intent.getIntExtra(FEED_ID, -1)
+        Log.d(TAG, "getFeedIdFromExtra: $feedId")
     }
 
     companion object{
