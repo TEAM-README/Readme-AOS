15d3f375352f2704624b8dfc962fdef8d3707db2
changhwan <chang5hustle@gmail.com>
2022-08-23 19:23:25 +0900

    [feat] Feed_Write_1 / ChooseCategoryFragment Ui 작업 (#80)

--- feature/write-feed/src/main/java/com/readme/android/write_feed/fragments/ChooseCategoryFragment.kt
+++ feature/write-feed/src/main/java/com/readme/android/write_feed/fragments/ChooseCategoryFragment.kt
@@ -1,11 +1,20 @@
 package com.readme.android.write_feed.fragments
 
+import android.content.ContentValues
+import android.content.res.ColorStateList
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import androidx.core.view.get
+import androidx.core.view.size
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.replace
+import com.google.android.material.chip.Chip
 import com.readme.android.core_ui.base.BindingFragment
+import com.readme.android.core_ui.constant.Category
 import com.readme.android.core_ui.constant.FeedWriteFragmentList.IMPRESSIVE_SENTENCE
+import com.readme.android.core_ui.ext.getColor
+import com.readme.android.shared.R.color
 import com.readme.android.write_feed.FeedWriteViewModel
 import com.readme.android.write_feed.R
 import com.readme.android.write_feed.databinding.FragmentChooseCategoryBinding
@@ -20,6 +29,8 @@ class ChooseCategoryFragment :
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initNextButtonClickListener()
+        setChips()
+        onClickChip()
     }
 
     private fun initNextButtonClickListener() {
@@ -27,6 +38,39 @@ class ChooseCategoryFragment :
             feedWriteViewModel.updateCurrentFragment(IMPRESSIVE_SENTENCE)
             requireActivity().supportFragmentManager.beginTransaction()
                 .replace<ImpressiveSentenceFragment>(R.id.container_feed_write).commit()
+        }
+    }
+
+    private fun setChips() {
+        val states = arrayOf(
+            intArrayOf(android.R.attr.state_selected),
+            intArrayOf(-android.R.attr.state_selected)
+        )
+        val backgroundColors = intArrayOf(getColor(color.chip_blue), getColor(color.chip_gray))
+        val textColors = intArrayOf(getColor(color.main_blue), getColor(color.gray_2))
+        val backgroundStateList = ColorStateList(states, backgroundColors)
+        val textStateList = ColorStateList(states, textColors)
+
+        for (i in 0 until Category.values().size) {
+            binding.chipGroup.addView(
+                Chip(requireContext()).apply {
+                    this.text = Category.values()[i].categoryName
+                    this.chipStartPadding = 35F
+                    this.chipEndPadding = 35F
+                    this.chipBackgroundColor = backgroundStateList
+                    this.setTextColor(textStateList)
+                    this.setTextAppearance(com.readme.android.shared.R.style.regular_01)
+                    this.setRippleColorResource(android.R.color.transparent)
+                }
+            )
+        }
+    }
+
+    private fun onClickChip() {
+        for (i in 0 until binding.chipGroup.size) {
+            binding.chipGroup[i].setOnClickListener {
+                it.isSelected = !it.isSelected
+            }
         }
     }
 }
