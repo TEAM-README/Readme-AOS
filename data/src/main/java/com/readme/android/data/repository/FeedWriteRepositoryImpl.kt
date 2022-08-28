c0c19a8809eca9d17058a8ee4db8107b7711001a
changhwan <chang5hustle@gmail.com>
2022-08-29 02:16:55 +0900

    [feat] Feed_Write_4 / PostFeed repository까지 작성(#104)

--- data/src/main/java/com/readme/android/data/repository/FeedWriteRepositoryImpl.kt
+++ data/src/main/java/com/readme/android/data/repository/FeedWriteRepositoryImpl.kt
@@ -1,12 +1,47 @@
 package com.readme.android.data.repository
 
+import com.readme.android.core_data.exception.RetrofitFailureStateException
 import com.readme.android.data.local.datasource.LocalPreferenceUserDataSource
+import com.readme.android.data.remote.calladapter.NetworkState
+import com.readme.android.data.remote.datasource.RemotePostFeedDataSource
+import com.readme.android.data.remote.model.request.PostFeedRequest
+import com.readme.android.domain.entity.request.DomainPostFeedRequest
+import com.readme.android.domain.entity.response.DomainHomeFeedResponse
 import com.readme.android.domain.repository.FeedWriteRepository
+import timber.log.Timber
+import java.security.cert.CertificateException
 import javax.inject.Inject
 
 class FeedWriteRepositoryImpl @Inject constructor(
-    private val localPreferenceUserDataSource: LocalPreferenceUserDataSource
+    private val localPreferenceUserDataSource: LocalPreferenceUserDataSource,
+    private val remotePostFeedDataSource: RemotePostFeedDataSource
 ) : FeedWriteRepository {
     override fun getUserNickName(): String =
         localPreferenceUserDataSource.getUserNickname()
+
+    override suspend fun postFeed(postFeedRequest: DomainPostFeedRequest): Result<Boolean> {
+        when (
+            val response = remotePostFeedDataSource.postFeed(
+                PostFeedRequest(
+                    categoryName = postFeedRequest.categoryName,
+                    impressiveSentence = postFeedRequest.impressiveSentence,
+                    feeling = postFeedRequest.feeling,
+                    bookInfo = postFeedRequest.bookInfo
+                )
+            )
+        ) {
+            is NetworkState.Success -> return Result.success(response.body.success)
+            is NetworkState.Failure ->
+                if (response.code == 401) throw CertificateException("토큰 만료 오류")
+                else return Result.failure(
+                    RetrofitFailureStateException(response.error, response.code)
+                )
+
+            is NetworkState.NetworkError -> Timber.tag("${this.javaClass.name}_postFeed")
+                .d(response.error)
+            is NetworkState.UnknownError -> Timber.tag("${this.javaClass.name}_postFeed")
+                .d(response.t)
+        }
+        return Result.failure(IllegalStateException("NetworkError or UnKnownError please check timber"))
+    }
 }
