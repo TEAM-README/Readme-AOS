4c3fb17dca32b454dfa59555847e0a757c715557
changhwan <chang5hustle@gmail.com>
2022-08-29 17:23:05 +0900

    [feat] Feed_Write_4 / RegisterDoneActivity 버튼 리스너 구현 (#104)

--- feature/write-feed/src/main/java/com/readme/android/write_feed/postfeed/RegisterDoneActivity.kt
+++ feature/write-feed/src/main/java/com/readme/android/write_feed/postfeed/RegisterDoneActivity.kt
@@ -2,13 +2,23 @@ package com.readme.android.write_feed.postfeed
 
 import android.os.Bundle
 import com.readme.android.core_ui.base.BindingActivity
+import com.readme.android.navigator.MainNavigator
 import com.readme.android.write_feed.R
 import com.readme.android.write_feed.databinding.ActivityRegisterDoneBinding
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 class RegisterDoneActivity :
     BindingActivity<ActivityRegisterDoneBinding>(R.layout.activity_register_done) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        initNextButtonClickListener()
+    }
+
+    private fun initNextButtonClickListener() {
+        binding.btnNext.setOnClickListener {
+            finish()
+        }
     }
 }
