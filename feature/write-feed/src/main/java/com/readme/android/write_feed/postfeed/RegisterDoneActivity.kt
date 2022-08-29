35ac9f0bb2a3a8bd6db071fb3c75104e3ac05cc8
changhwan <chang5hustle@gmail.com>
2022-08-29 14:28:48 +0900

    [feat] Feed_Write_4 / RegisterDoneActivity UI 구현 (#104)

--- feature/write-feed/src/main/java/com/readme/android/write_feed/postfeed/RegisterDoneActivity.kt
+++ feature/write-feed/src/main/java/com/readme/android/write_feed/postfeed/RegisterDoneActivity.kt
@@ -1,12 +1,14 @@
 package com.readme.android.write_feed.postfeed
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.readme.android.core_ui.base.BindingActivity
 import com.readme.android.write_feed.R
+import com.readme.android.write_feed.databinding.ActivityRegisterDoneBinding
 
-class RegisterDoneActivity : AppCompatActivity() {
+class RegisterDoneActivity :
+    BindingActivity<ActivityRegisterDoneBinding>(R.layout.activity_register_done) {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register_done)
     }
 }
