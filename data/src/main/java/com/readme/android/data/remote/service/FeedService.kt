8212342721fe628b7611cd49ca61385afeebd50b
changhwan <chang5hustle@gmail.com>
2022-08-30 17:36:26 +0900

    [chore] FeedService / disable ktLint

--- data/src/main/java/com/readme/android/data/remote/service/FeedService.kt
+++ data/src/main/java/com/readme/android/data/remote/service/FeedService.kt
@@ -6,8 +6,8 @@ import com.readme.android.data.remote.model.response.BaseResponse
 import com.readme.android.data.remote.model.response.DetailFeedResponse
 import com.readme.android.data.remote.model.response.HomeFeedResponse
 import com.readme.android.data.remote.model.response.PostFeedResponse
-import retrofit2.http.*
 import com.readme.android.data.remote.model.response.NoDataResponse
+import retrofit2.http.* // ktlint-disable no-wildcard-imports
 
 interface FeedService {
 
