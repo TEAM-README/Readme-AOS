384f57d3f438026592d5109bee8a2142aaafd85b
mdb1217 <70698151+mdb1217@users.noreply.github.com>
2022-09-08 07:52:34 +0900

    [add] mypage_setting / 암시적 인텐트 관련 화면전환 함수 추가 ( #59 , #124 )

--- core-ui/src/main/java/com/readme/android/core_ui/ext/ContextExtension.kt
+++ core-ui/src/main/java/com/readme/android/core_ui/ext/ContextExtension.kt
@@ -4,10 +4,14 @@ import android.content.Context
 import android.app.Activity
 import android.content.Context.INPUT_METHOD_SERVICE
 import android.content.Intent
+import android.content.Intent.ACTION_SEND
+import android.content.Intent.EXTRA_EMAIL
+import android.net.Uri
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
 import androidx.core.os.bundleOf
+import com.readme.android.shared.R
 
 fun Context.showKeyboard(view: View) {
     val inputMethodManager = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
@@ -33,4 +37,27 @@ inline fun <reified T : Activity> Context.navigateActivity(
     vararg argument: Pair<String, Any?>
 ) {
     startActivity(buildIntent<T>(*argument))
+}
+
+fun Context.startUri(
+    url: String,
+    vararg argument: Pair<String, Any?>
+) {
+    startActivity(
+        Intent(Intent.ACTION_VIEW, Uri.parse(url)).apply {
+            putExtras(bundleOf(*argument))
+        }
+    )
+}
+
+fun Context.startMail(
+    vararg argument: Pair<String, Any?>
+) {
+    startActivity(
+        Intent(ACTION_SEND).apply {
+            type = "plain/text"
+            putExtra(EXTRA_EMAIL, resources.getStringArray(R.array.email_array))
+            putExtras(bundleOf(*argument))
+        }
+    )
 }
